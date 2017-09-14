f73b42074a657d0fe71837d1bc4c89b7f6a439fc
Oskar <juksar98@users.noreply.github.com>
2017-09-14 11:31:15 +0300

    Lisatud muutujad ja if lause

--- esimene.php
+++ esimene.php
@@ -1,3 +1,23 @@
+<?php
+	//muutujad
+	$myName = "Oskar";
+	$myFamilyName = "Juksar";
+	
+	//hindan päeva osa, (võrdlemine < > <= >= == !=)
+	$hourNow = date("H");
+	$partOfDay = "";
+	if ($hourNow < 8){
+		$partOfDay = "varajane hommik";
+	}	
+	if ($hourNow >= 8 and < 16){
+		$partOfDay = "koolipäev";
+	}	
+	if ($hourNow > 16){
+		$partOfDay = "vaba aeg";
+	}
+	//echo $partOfDay;
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -5,12 +25,18 @@
 	<title>Oskar Juksar</title>
 </head>
 <body>
-	<h1>Oskar Juksar</h1>
+	<h1><?php echo $myName ." " .$myFamilyName; ?></h1>
 	<p>See veebileht on loodud õppetöö raames ning ei sisalda mingisugust tõsiseltvõetavat sisu!</p>
 	<p>Ei tule pähe hetkel mitte ühtegi loomingulist ideed, mida siia kirjutada, aga tere olen Oskar Juksar
-	ja see on minu esimene veebileht.<p>
+	ja see on minu esimene veebileht.</p>
 	<?php
-		echo "Algas php õppimine";
+		echo "<p>Algas php õppimine</p>";
+		echo "<p>Täna on ";
+		echo date("d.m.Y") .", kell oli lehe avamise hetkel " .date("H:i:s");
+		echo ", hetkel on " .$partOfDay .".</p>";
+		
 	?>
+	
+	
 </body>
 </html>
