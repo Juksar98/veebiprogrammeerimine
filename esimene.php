8938d949a91d20a14e14639fd82081af38ee68e4
Oskar <juksar98@users.noreply.github.com>
2017-09-14 10:52:39 +0300

    Lisatud on esimene php jupp

---
+++ esimene.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset"utf-8">
+	<title>Oskar Juksar</title>
+</head>
+<body>
+	<h1>Oskar Juksar</h1>
+	<p>See veebileht on loodud õppetöö raames ning ei sisalda mingisugust tõsiseltvõetavat sisu!</p>
+	<p>Ei tule pähe hetkel mitte ühtegi loomingulist ideed, mida siia kirjutada, aga tere olen Oskar Juksar
+	ja see on minu esimene veebileht.<p>
+	<?php
+		echo "Algas php õppimine";
+	?>
+</body>
+</html>
